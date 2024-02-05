305aaec6bb451dfa93bdfeb159ee5717fc650de5
DevangMagarkar <141154115+DevangMagarkar@users.noreply.github.com>
2024-02-05 08:50:41 +0000

    Conversion of a dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "devang@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chhindwara" //variable can be declared like this but it is not good
+
+let acccountState
+
+// accountId = 2 //not allowed
+
+console.log(accountId);
+
+accountEmail ="abc@123.com"
+accountPassword = "243234"
+accountCity ="Betul"
+console.log(accountId);
+
+console.table([accountEmail , accountId , accountPassword , accountCity , acccountState])
+
+/*
+Prefer not to use var because of issue in log scope and functional scope
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+ let name ="Devang"
+ let age = 18
+ let isLoggedIn= true
+
+ //number => 2 to power 53
+ // big int
+ // string => ""
+ //null => standalone value object
+ //undefined => undefined
+ //symbol => unique
+
+ console.log(typeof age);
+ console.log(typeof name);
+ console.log(typeof null);
+ console.log(typeof undefined);
