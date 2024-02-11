49d74ad2308a743ba82f42ac2f1f7c6f1082812c
DevangMagarkar <141154115+DevangMagarkar@users.noreply.github.com>
2024-02-12 00:22:37 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -14,3 +14,29 @@ const dc_heros =["flash", "superman", "batman"]
 const all_new_heros = [...marvel_heros,...dc_heros]
 //console.log(all_new_heros);
 
+//to convert nested array into flat array
+//flat keyword is used
+
+const anotherarray =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const realAnotherArray = anotherarray.flat(Infinity)
+console.log(realAnotherArray)
+
+//to check whether it is an array or not
+console.log(Array.isArray("Devang"))
+
+//to conver a string into an array of characters
+console.log(Array.from("Devang"))
+
+//object cannot be cnverted from this method
+console.log(Array.from({name: "Devang"}))
+
+//to convert a given number of variables into an array
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
